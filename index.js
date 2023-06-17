c02f73739b8236f36252e5b3c2caa54f0ee1ec7d
spalkumar <rcsupal@gmail.com>
2023-06-17 11:18:04 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
